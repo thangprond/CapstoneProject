0838eac91c541b3f0135d4e6ab4703717ea583bf
tranvthang <thangprond97@gmail.com>
2019-09-12 10:40:21 +0700

    cmt

--- FPT-Libol-master/Libol/Libol/Startup.cs
+++ FPT-Libol-master/Libol/Libol/Startup.cs
@@ -18,7 +18,7 @@ namespace Libol
             //GlobalConfiguration.Configuration.UseSqlServerStorage(@"data source =VAN_SANG\SQLEXPRESS ; initial catalog = Libol; integrated security = True;");
             app.UseHangfireDashboard();
             OverdueEmailNoticeController obj = new OverdueEmailNoticeController();
-            RecurringJob.AddOrUpdate(() => obj.SendEmail("nguyennm2@fe.edu.vn"), Cron.Daily);
+            //RecurringJob.AddOrUpdate(() => obj.SendEmail("nguyennm2@fe.edu.vn"), Cron.Daily);
             //RecurringJob.AddOrUpdate(() => obj.SendEmail("hongnhat97tt@gmail.com"), Cron.Hourly);
             app.UseHangfireServer();
         }
